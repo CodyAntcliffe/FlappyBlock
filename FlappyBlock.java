2d76c9d54d8eede3dd48123eed9ce2905710ce5e
Cody Antcliffe <codyantcliffe@gmail.com>
2014-06-03 15:34:34 -0400

    Update FlappyBlock.java

--- FlappyBlock.java
+++ FlappyBlock.java
@@ -17,33 +17,29 @@ public class FlappyBlock extends JPanel implements MouseListener {
 	//frame dimensions
 	int windowHeight=600;//frame height
 	int windowWidth=800;//frame width
-	
 	//Various colors used
 	Color bgColor=Color.CYAN;
 	Color blockyColor=Color.RED;
 	Color pipeColor=Color.GREEN;
 	
-	Pipes P=new Pipes(); //TEST TO REMOVE
-	Pipes P2=new Pipes();
 	Pipes[] PY=new Pipes[100];//holds our instance of pipes
 	
-	//Starting location for flappy
+	//Starting location for blocky
 	int blockyX=300;
 	int blockyY=200;
-
-	int blockyDimensions=15;
-	int pipeX=windowWidth-100;
-	int pipeY=0;
+	int blockyDimensions=15;//sets size for blocky
 	
+	//These are used to control flying up and down
 	flyUp up=new flyUp();
 	flyDown down=new flyDown();
 	Timer flyUpTimer=new Timer(10,up);
 	Timer flyDownTimer=new Timer(10,down);
 	int timerKeep=0;
 	
-	
+	//constructor
 	public FlappyBlock() {
-		for(int i=0; i<100;i++){
+		//initializes the pipes
+		for(int i=0; i<PY.length;i++){
 			PY[i]=new Pipes();
 			PY[i].topPipeX=(windowWidth-100)+(i*250);
 			PY[i].bottomPipeX=PY[i].topPipeX;
@@ -53,7 +49,6 @@ public class FlappyBlock extends JPanel implements MouseListener {
 		setBackground(bgColor);
 		setFocusable(true);
 		setPreferredSize(new Dimension(windowWidth,windowHeight));
-		//setPipeX(PY);
 	}
 	
 	public void drawPipes(Pipes[] PZ){
@@ -63,7 +58,7 @@ public class FlappyBlock extends JPanel implements MouseListener {
 	public class flyUp implements ActionListener{
 		public void actionPerformed(ActionEvent event){
 			if(timerKeep<14){
-				for(int i=0;i<100;i++)
+				for(int i=0;i<PY.length;i++)
 					PY[i].movePipes();
 			blockyY-=6;
 			timerKeep++;
@@ -78,7 +73,7 @@ public class FlappyBlock extends JPanel implements MouseListener {
 		public class flyDown implements ActionListener{
 			public void actionPerformed(ActionEvent event){
 				if(timerKeep>=10){
-					for(int i=0;i<100;i++)
+					for(int i=0;i<PY.length;i++)
 						PY[i].movePipes();
 				blockyY+=6;
 				timerKeep++;
@@ -89,11 +84,11 @@ public class FlappyBlock extends JPanel implements MouseListener {
 					return;}
 			}
 		}
+	//class for the pipe objects
 	private class Pipes{
-		int topPipeX=windowWidth-100;
+		int topPipeX=windowWidth-100;//States where to place first pipe on X-axis
 		int topPipeY=0;
 		int topPipeHeight=getRandomPipeHeight();//controls how tall the top pipe will be
-		
 		int bottomPipeX=topPipeX;
 		int bottomPipeY=topPipeHeight+170;//gives an opening between the pipes of 200
 		int bottomPipeHeight=windowHeight+20;//will make the pipe touch the bottom of the frame
@@ -116,7 +111,7 @@ public class FlappyBlock extends JPanel implements MouseListener {
 	public void paintComponent(Graphics G){
 		super.paintComponent(G);
 		paintFlappy(G);
-		for(int i=0;i<100;i++)
+		for(int i=0;i<PY.length;i++)
 		{
 			paintPipes(G,PY[i]);
 		}
@@ -125,7 +120,6 @@ public class FlappyBlock extends JPanel implements MouseListener {
 	//Outlines the pipe in black lines to give it a more defined look
 	private void outlinePipes(Graphics G, Pipes P){
 		G.setColor(Color.BLACK);
-		
 		//outlines top pipe
 		G.drawLine(P.topPipeX, P.topPipeY, P.topPipeX,  P.topPipeY+P.topPipeHeight-20);
 		G.drawLine(P.topPipeX+100, P.topPipeY,P.topPipeX+100,  P.topPipeY+P.topPipeHeight-20);
@@ -133,7 +127,6 @@ public class FlappyBlock extends JPanel implements MouseListener {
 		G.drawLine(P.topPipeX-5, P.topPipeY+P.topPipeHeight, P.topPipeX+105,  P.topPipeY+P.topPipeHeight);
 		G.drawLine(P.topPipeX-5, P.topPipeY+P.topPipeHeight-20, P.topPipeX-5,  P.topPipeY+P.topPipeHeight);
 		G.drawLine(P.topPipeX+105, P.topPipeY+P.topPipeHeight-20, P.topPipeX+105,  P.topPipeY+P.topPipeHeight);
-		
 		//outlines bottom pipe
 		G.drawLine(P.bottomPipeX, P.bottomPipeY+20, P.bottomPipeX, P.bottomPipeHeight);
 		G.drawLine(P.bottomPipeX+100, P.bottomPipeY+20,P.bottomPipeX+100, P.bottomPipeHeight);
@@ -141,27 +134,23 @@ public class FlappyBlock extends JPanel implements MouseListener {
 		G.drawLine(P.bottomPipeX-5, P.bottomPipeY, P.bottomPipeX+105, P.bottomPipeY);
 		G.drawLine(P.bottomPipeX-5, P.bottomPipeY+20, P.bottomPipeX-5, P.bottomPipeY);
 		G.drawLine(P.bottomPipeX+105, P.bottomPipeY+20, P.bottomPipeX+105, P.bottomPipeY);
-	
 	}
 	//Draws a pipe 
 	private void paintPipes(Graphics G, Pipes p){
 		G.setColor(pipeColor);
 		G.fillRect(p.topPipeX, p.topPipeY, 100, p.topPipeHeight);
-		G.fillRect(p.topPipeX-5, p.topPipeHeight-20, 110, 20);
+		G.fillRect(p.topPipeX-5, p.topPipeHeight-20, 110, 20);		
 		
 		//Draws bottom pipe
-		G.fillRect(p.bottomPipeX, p.bottomPipeY, 100, P.bottomPipeHeight);
+		G.fillRect(p.bottomPipeX, p.bottomPipeY, 100, p.bottomPipeHeight);
 		G.fillRect(p.bottomPipeX-5, p.bottomPipeY, 110, 20);
-		outlinePipes(G,p);
-	
+		outlinePipes(G,p);	
 	}
-	
 	//Draws flappy
 	private void paintFlappy(Graphics G){
 		G.setColor(blockyColor);
 		G.fillRect(blockyX,blockyY, blockyDimensions, blockyDimensions);
 	}
-	
 	//When mouse is clicked
 		public void mousePressed(MouseEvent evt)
 		{
@@ -173,6 +162,7 @@ public class FlappyBlock extends JPanel implements MouseListener {
 				flyUpTimer.start();
 				repaint();
 		}
+
 
 		//suppresses a warning
 		public void mouseClicked(MouseEvent arg0) {
